20164d21cc42e706b3f85cbef3766acbb20ccf06
Theryston <funktodo2@gmail.com>
2022-05-19 22:29:39 -0300

    fix: return json

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ exports.handler = async (event) => {
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(instagramUser),
+    userInstagramData: instagramUser,
   };
 
   return response;
